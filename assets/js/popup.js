395b182a1b2cf254dc9a50eabb28e72003feb971
Shubham Nishad <shubhamnishad25@gmail.com>
2017-07-02 18:44:37 +0530

    made the qrcode generator responsive to input change.

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,4 +1,24 @@
-jQuery(function(){
-  var input = jQuery('#input').text();
-  jQuery('#output').qrcode(input);
-})
+function qr_create() {
+  var text = '';
+  text = $('textarea#input').val();
+  if(text.length > 1276 || text == '') {
+    text = chrome.i18n.getMessage('invalid_text');
+  }
+  console.log(text)
+  $('#output').empty();
+  $('#output').qrcode({
+    // width: 256,
+    // height: 256,
+    // correctLevel: 0,
+    text: text
+  });
+}
+
+$(function(){
+  qr_create();
+});
+
+$(document).ready(function(){
+  $('#create').on('click', qr_create);
+  $("#input").on('change keyup paste', qr_create);
+});
